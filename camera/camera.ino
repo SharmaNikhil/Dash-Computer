f6fb3ed5847e1583787d24f534ca9a4d08e668ff
Nikhil Sharma <nikhil@Pyotr.local>
2015-02-19 09:55:11 -0500

    (Untested) completed camera code

--- camera/camera.ino
+++ camera/camera.ino
@@ -4,9 +4,9 @@ int pin_out_ind_g = 10;
 int pin_out_ind_r = 11;
 int pin_in_rf = 9; //rf signal
 
-int time_cycle_wait = 50; //time between cycles
-int time_rf_on = 50;
-int time_rf_off = 1000-time_rf_on-cycle_wait_time;
+int time_cycle = 500; //time per cycles
+int time_rled_on = 250;
+int time_rled_off = time_cycle-time_rled_on;
 int time_switch_trigger = 5000; //time to trigger switch, SET TO 5 SECONDS FOR TESTING, SET TO 60 FOR FINAL
 
 void setup() {
@@ -15,11 +15,36 @@ void setup() {
   pinMode(pin_out_rf,OUTPUT);
   pinMode(pin_out_ind_g,OUTPUT);
   pinMode(pin_out_ind_r,OUTPUT);
-  pinMode(pin_in,rf,INPUT);
+  pinMode(pin_in_rf,INPUT);
+  
+  Serial.begin(9600);
 }
 
+int input = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-  //1 loop = 1 second
+  digitalWrite(pin_out_rf,HIGH);
+  Serial.println("RF on");
+  input=digitalRead(pin_in_rf);
+  Serial.println("RF val:"+input);
+  digitalWrite(pin_out_rf,LOW);
+  Serial.println("RF off");
   
+  if(input){
+    Serial.println("input on");
+    digitalWrite(pin_out_ind_g,HIGH);
+    digitalWrite(pin_out_switch);
+    Serial.println("pin_out_switch on");
+    delay(time_switch_trigger);
+    digitalWrite(pin_out_ind_g,LOW);
+    digitalWrite(pin_out_switch,LOW);
+    Serial.println("pin_out_switch off");
+  }
+  else{
+    Serial.println("input off");
+    digitalWrite(pin_out_ind_r,HIGH);
+    delay(time_rled_on);
+    digitalWrite(pin_out_ind_r,LOW);
+    delay(time_rled_off);
+  }
 }
